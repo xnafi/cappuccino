6b91e972928e99a0e2afa8f634b001ee1cb28f14
Forhad Khan <87197002+xnafi@users.noreply.github.com>
2024-06-29 16:59:01 +0600

    table book form done

--- src/components/view/TableBook/TableBook.tsx
+++ src/components/view/TableBook/TableBook.tsx
@@ -1,9 +1,14 @@
+"use client";
+import React, { useState } from "react";
 import FadeIn from "@/utils/FadeIn";
 import SelectComponent from "@/utils/SelectComponent";
 import { MdGroups2 } from "react-icons/md";
-import styles from './Table.module.css';
+import { MdOutlineAccessTime } from "react-icons/md";
+import Swal from "sweetalert2";
 
 export default function TableBook() {
+  const Swal = require("sweetalert2");
+  // persons option
   const options = [
     "1 Person",
     "2 People",
@@ -17,17 +22,109 @@ export default function TableBook() {
     "10 People",
     "12 People",
   ];
+  // time option
+  const timeOptions = [
+    "7:30 AM",
+    "8:00 AM",
+    "8:30 AM",
+    "9:00 AM",
+    "9:30 AM",
+    "10:00 AM",
+    "10:30 AM",
+    "11:00 AM",
+    "11:30 AM",
+    "12:00 PM",
+    "12:30 PM",
+    "1:00 PM",
+    "1:30 PM",
+    "2:00 PM",
+    "2:30 PM",
+    "3:00 PM",
+    "3:30 PM",
+    "4:00 PM",
+    "4:30 PM",
+    "5:00 PM",
+    "5:30 PM",
+    "6:00 PM",
+    "6:30 PM",
+    "7:00 PM",
+    "7:30 PM",
+    "8:00 PM",
+    "8:30 PM",
+    "9:00 PM",
+    "9:30 PM",
+    "10:00 PM",
+    "10:30 PM",
+    "11:00 PM",
+    "11:30 PM",
+    "12:00 AM",
+  ];
+
+  // cant select previous date
   const getTodayDate = () => {
     const today = new Date();
     const yyyy = today.getFullYear();
-    const mm = String(today.getMonth() + 1).padStart(2, "0"); // Months are zero-based
+    const mm = String(today.getMonth() + 1).padStart(2, "0");
     const dd = String(today.getDate()).padStart(2, "0");
     return `${yyyy}-${mm}-${dd}`;
   };
+
+  const [formData, setFormData] = useState({
+    people: options[0],
+    date: getTodayDate(),
+    time: timeOptions[0],
+  });
+
+  const handleSelectChange =
+    (field: string) => (event: React.ChangeEvent<HTMLSelectElement>) => {
+      setFormData({
+        ...formData,
+        [field]: event.target.value,
+      });
+    };
+
+  const handleDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      date: event.target.value,
+    });
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log("Form data submitted:", formData);
+    Swal.fire({
+      title: "You Are Selected",
+      text: `${formData.people} Date Is
+       ${formData.date} And 
+       The Time Is ${formData.time}`,
+      icon: "question",
+
+    });
+    Swal.fire({
+      title: "You Are Selected",
+      text: `${formData.people} Date Is
+       ${formData.date} And 
+       The Time Is ${formData.time}`,
+      icon: "question",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`,
+    }).then((result: { isConfirmed: any; isDenied: any }) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        Swal.fire("Booking Done", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+    
+  };
+
   return (
-    <div className="bg-[#151412] flex h-full lg:h-screen w-screen">
-      <div className="max-w-[1400px] mx-auto flex justify-center items-center flex-col space-y-2 px-2 md:px-4 my-10 lg:my-0">
-        {/* headings */}
+    <div className="bg-[#151412] flex h-full lg:h-screen w-full px-2">
+      <div className="w-full lg:max-w-[1400px] mx-auto flex justify-center items-center flex-col space-y-2 px-2 md:px-4 my-10 lg:my-0">
         <div className="space-y-2 text-center !w-full">
           <FadeIn delay={0.5}>
             <span className="primary-text !text-white/80 text-center">
@@ -38,24 +135,47 @@ export default function TableBook() {
             <span className="headings">Book Your Table</span>
           </FadeIn>
         </div>
-        {/* inputs */}
-        <div className="flex flex-col md:flex-row w-[100%] items-center gap-10 border justify-between !mt-10">
-          <SelectComponent options={options} icon={<MdGroups2 />} />
+        {/* form */}
+        <form
+          className="flex flex-col md:flex-row w-[100%] items-center gap-10 justify-between !mt-10"
+          onSubmit={handleSubmit}
+        >
+          {/* select persons */}
+          <SelectComponent
+            options={options}
+            icon={<MdGroups2 />}
+            value={formData.people}
+            onChange={handleSelectChange("people")}
+          />
           <span className="sub-heading font-bold">FOR</span>
-          <div className={styles["custom-date-input"]}>
+          {/* select date */}
+          <div
+            className={`styles["custom-date-input"] border py-4 px-4 w-full`}
+          >
             <input
               type="date"
-              className="w-full bg-[#151412] text-white"
+              className="w-full !bg-[#151412] !text-white focus:outline-none cursor-pointer"
               min={getTodayDate()}
+              value={formData.date}
+              onChange={handleDateChange}
             />
           </div>
-
           <span className="sub-heading font-bold">AT</span>
-          <SelectComponent options={options} icon={<MdGroups2 />} />
-          <button className="cursor-pointer uppercase bg-[#6F4E37] font-bold px-10 py-2 border transition-all duration-300 ease-in-out hover:shadow-md transform hover:scale-105 !z-0">
-            EXPLORE
+          {/* select time */}
+          <SelectComponent
+            options={timeOptions}
+            icon={<MdOutlineAccessTime />}
+            value={formData.time}
+            onChange={handleSelectChange("time")}
+          />
+          <button
+            type="submit"
+            className="cursor-pointer uppercase bg-[#6F4E37] font-bold px-10 py-2 border transition-all duration-300 ease-in-out hover:shadow-md transform hover:scale-105 !z-0"
+          >
+            SUBMIT
           </button>
-        </div>
+        </form>
+        {/*  */}
       </div>
     </div>
   );
--- src/utils/SelectComponent.tsx
+++ src/utils/SelectComponent.tsx
@@ -3,12 +3,23 @@ import React from "react";
 interface SelectComponentProps {
   options: string[];
   icon: React.ReactNode;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const SelectComponent: React.FC<SelectComponentProps> = ({ options, icon }) => {
+const SelectComponent: React.FC<SelectComponentProps> = ({
+  options,
+  icon,
+  value,
+  onChange,
+}) => {
   return (
-    <div className="relative group rounded-lg w-full overflow-hidden flex items-center border gap-4 px-4">
-      <select className="select w-full select-bordered focus:outline-none py-4">
+    <div className="relative group rounded-lg w-full overflow-hidden flex items-center border gap-4 px-4 bg-neutral-700">
+      <select
+        className="select w-full select-bordered focus:outline-none py-4 appearance-none"
+        value={value}
+        onChange={onChange}
+      >
         {options.map((option, index) => (
           <option key={index} className="">
             {option}
